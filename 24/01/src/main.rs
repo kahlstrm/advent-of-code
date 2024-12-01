0f80374e72fb3edad13b42a3e3631cc1aa8adbe2
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2024-12-01 23:17:10 +0200

    feat: template and start script, 24 day 1

---
+++ 24/01/src/main.rs
@@ -0,0 +1,37 @@
+static TEST_INPUT: &str = r#"
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+"#;
+static INPUT: &str = include_str!("input");
+
+fn main() {
+    let (mut left, mut right): (Vec<_>, Vec<_>) = if INPUT.len() == 0 { TEST_INPUT } else { INPUT }
+        .lines()
+        .filter_map(|l| match l.trim() {
+            "" => None,
+            other => Some(other.split_whitespace()),
+        })
+        .map(|mut l| {
+            let first = l.next().unwrap().parse::<i32>().unwrap();
+            let second = l.next().unwrap().parse::<i32>().unwrap();
+            (first, second)
+        })
+        .unzip();
+    left.sort();
+    right.sort();
+    let res1: i32 = left
+        .iter()
+        .zip(right.clone())
+        .map(|(a, b)| (a - b).abs())
+        .sum();
+    let res2: i32 = left
+        .iter()
+        .map(|v| v * (right.iter().filter(|b| **b == *v).count() as i32))
+        .sum();
+    println!("{res1:#?}");
+    println!("{res2:#?}");
+}
---
+++ template/src/main.rs
@@ -0,0 +1,12 @@
+static TEST_INPUT: &str = r#""#;
+static INPUT: &str = include_str!("input");
+
+fn main() {
+    let lines = if INPUT.len() == 0 { TEST_INPUT } else { INPUT }
+        .lines()
+        .filter_map(|l| match l.trim() {
+            "" => None,
+            other => Some(other),
+        });
+    println!("{lines:#?}")
+}
