c8b21ac196230a355546729650bea6840dfec485
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2024-12-06 20:37:17 +0200

    feat(24/06): add parallellism

--- 24/06/src/main.rs
+++ 24/06/src/main.rs
@@ -1,3 +1,5 @@
+use std::{num::NonZero, sync::Arc, thread::JoinHandle};
+
 use matrixwalkerer::Direction;
 
 static TEST_INPUT: &[u8] = br#"....#.....
@@ -64,22 +66,22 @@ fn traverse_matrix(
     };
     return is_looped;
 }
-
 fn main() {
-    let matrix: Vec<&[u8]> = if INPUT.len() == 0 { TEST_INPUT } else { INPUT }
+    let matrix: Arc<[&[u8]]> = if INPUT.len() == 0 { TEST_INPUT } else { INPUT }
         .split(|c| *c == b'\n')
         .filter(|l| l.len() != 0)
         .collect();
 
-    let start_pos = matrix
+    let start_pos: Arc<(usize, usize)> = matrix
         .iter()
         .position(|arr| arr.iter().find(|c| **c == b'^').is_some())
         .map(|j| (matrix[j].iter().position(|c| *c == b'^').unwrap(), j))
-        .unwrap();
+        .unwrap()
+        .into();
     let mut visited: Vec<Vec<Option<Direction>>> = (0..matrix.len())
         .map(|j| vec![None; matrix[j].len()])
         .collect();
-    let _ = traverse_matrix(&matrix, start_pos, &mut visited, None);
+    traverse_matrix(&matrix, *start_pos, &mut visited, None);
     let visited_positions: Vec<(usize, usize)> = visited
         .iter()
         .enumerate()
@@ -92,16 +94,33 @@ fn main() {
                 })
         })
         .collect();
-    println!("{}", visited_positions.len());
+    let visited_count = visited_positions.len();
+    println!("{}", visited_count);
+    let cpu_count = std::thread::available_parallelism().unwrap_or(NonZero::new(1).unwrap());
     let looped_count = visited_positions
+        .chunks(visited_count / cpu_count)
+        .map(|c| c.to_owned())
         .into_iter()
-        .filter(|(i, j)| {
-            let cur = matrix[*j][*i];
-            if cur != b'.' {
-                return false;
-            }
-            return traverse_matrix(&matrix, start_pos, &mut visited, (*i, *j).into());
+        .map(|arr| {
+            let matrix = matrix.clone();
+            let start_pos = start_pos.clone();
+            std::thread::spawn(move || {
+                let mut visited: Vec<Vec<Option<Direction>>> = (0..matrix.len())
+                    .map(|j| vec![None; matrix[j].len()])
+                    .collect();
+                arr.iter()
+                    .filter(|(i, j)| {
+                        let cur = matrix[*j][*i];
+                        if cur != b'.' {
+                            return false;
+                        }
+                        return traverse_matrix(&matrix, *start_pos, &mut visited, (*i, *j).into());
+                    })
+                    .count()
+            })
         })
-        .count();
+        .collect::<Vec<JoinHandle<usize>>>()
+        .into_iter()
+        .fold(0, |acc, handle| acc + handle.join().unwrap());
     println!("{looped_count}");
 }
