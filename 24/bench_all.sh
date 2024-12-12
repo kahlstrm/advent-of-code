4647010f07a68839431d8df4a08edf42cc30c3b6
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2024-12-12 06:57:17 +0200

    feat: wip of benchmarks in README.md

--- 24/bench_all.sh
+++ 24/bench_all.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -eo pipefail
 
-ls src/bin | while read LINE; do
-BIN_NAME=${LINE%.rs}
-cargo b -q -r --bin $BIN_NAME
-hyperfine --warmup 5 -N target/release/$BIN_NAME
+OUTPUT_FILE="$1"
+MARKDOWN_ARG=()
+
+if [ -n "$OUTPUT_FILE" ]; then
+    MARKDOWN_ARG=(--export-markdown "$OUTPUT_FILE")
+fi
+
+commands=()
+names=()
+for file in src/bin/*.rs; do
+    BIN_NAME=$(basename "$file" .rs)
+    cargo build -q --release --bin "$BIN_NAME"
+    names+=("-nDay $((10#${BIN_NAME#day}))")
+    commands+=("target/release/$BIN_NAME")
 done
+
+hyperfine --warmup 5 -N "${names[@]}" "${commands[@]}" "${MARKDOWN_ARG[@]}"
